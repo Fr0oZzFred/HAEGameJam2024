6ad1b68f09059d3a5d28e98f4469ef5fe3ea827c
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2024-01-31 11:23:43 +0100

    destroyable obstacles + collision upgrade

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
+    [SerializeField] bool destroyableObstacle = false;
     public void MakeNoise(float intensity) {
         LevelManager.Instance.MakeNoise(intensity);
+        if (!destroyableObstacle) return;
+
+        GetComponent<Prop>().enabled = false;
+        GetComponent<Animator>().SetBool("Broken", true);
     }
 }
